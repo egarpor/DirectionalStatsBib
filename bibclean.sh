ec8199750bfc7d8e89deff5ee62f4921eda21a8f
Eduardo García-Portugués <egarpor@users.noreply.github.com>
2021-12-28 16:07:13 +0100

    Check for duplicates

--- bibclean.sh
+++ bibclean.sh
@@ -1,2 +1,2 @@
 # Download bibtex-tidy from https://github.com/FlamingTempura/bibtex-tidy
-./bibtex-tidy-master/bin/bibtex-tidy --sort=type,-year,author,key --no-escape --align=13 --curly --tab --sort-fields=title,subtitle,author,year,journal,fjournal,booktitle,booksubtitle,publisher,address,series,volume,number,pages,editor,edition,note,url,doi DirectionalStats.bib
+./bibtex-tidy-master/bin/bibtex-tidy --sort=type,-year,author,key --no-escape --align=13 --curly --tab --duplicates=citation --sort-fields=title,subtitle,author,year,journal,fjournal,booktitle,booksubtitle,publisher,address,series,volume,number,pages,editor,edition,note,url,doi --no-remove-dupe-fields DirectionalStats.bib
